8a5a8c2ae34622c481829c8af7a456c370edf07d
Braulio Lazo <yobraulio23@gmail.com>
2023-07-27 12:14:08 -0400

    Borrador ENTRADAS casi listo

---
+++ blog/entrada-uno/snippetLoader.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('/blog/entrada-uno/snippets/snippet1.txt')
+    .then(response => response.text())
+    .then(data => {
+        let preElement = document.querySelector('#snippet1');
+        preElement.textContent = data;
+    
+        // Haz que Prisma.js resalte el nuevo código
+        Prism.highlightElement(preElement);
+    })
+    .catch(error => console.error('Error:', error));
+});
