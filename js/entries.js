5a5ba487f856cc048e52a44a8985079184c8a882
Braulio Lazo <yobraulio23@gmail.com>
2023-07-27 12:41:38 -0400

    LImpieza de JavaScript en entradas

--- js/entries.js
+++ js/entries.js
@@ -1,17 +1,20 @@
-function copiarAlPortaPapeles() {
+function copyToClipboard() {
     document.querySelectorAll('.copy-button').forEach(button => {
         button.addEventListener('click', () => {
             const targetId = button.getAttribute('data-copy-target');
             const target = document.querySelector(targetId);
+
             navigator.clipboard.writeText(target.textContent)
-                .then(() => console.log('Copiado al portapapeles!'))
-                .catch(err => console.log('Error al copiar al portapapeles', err));
+                .then(() => {
+                    button.textContent = "Copied"; 
+                })
+                .catch(err => console.log('Error copying to clipboard', err));
+
+            setTimeout(() => {
+                button.textContent = "Copy"; 
+            }, 3000);  
         });
     });
 }
 
-
-window.addEventListener("DOMContentLoaded", () => {
-    copiarAlPortaPapeles();
-})
-
+window.addEventListener("DOMContentLoaded", copyToClipboard);
