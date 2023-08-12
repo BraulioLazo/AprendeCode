55f764582ffbb3d6e7ca74f393e955e86b722dbb
Braulio Lazo <yobraulio23@gmail.com>
2023-08-12 14:11:33 -0400

    Modificando la busqueda y SEO en CLIP-PATH

---
+++ js/blogEntries.js
@@ -0,0 +1,30 @@
+let entries = [
+    {
+        "title": "introduccion al clip-Path dando formas creativas a Tu pagina web",
+        "keywords": "frontend, all, clip-path, clip, path, diseño, web, formas, creativas, CSS, tutorial, guía, personalización, clippy, design"
+    },
+    {
+        "title": "Creación de un Menú Web: Guía Completa para Principiantes",
+        "keywords": "frontend, all, menú, menu, web, guía, guia, para, principiantes, diseño, web, construccion, creacion, de, menús, desarrollo, web, practicos"
+    },
+    {
+        "title": "creando columnas estilizadas en css una guia paso a paso",
+        "keywords": "frontend, all, columnas, column-count, column-gap, column-rule, web, estilos, diseño, responsivo, tutorial, css"
+    },
+    {
+        "title": "domina la propiedad object-fit en css",
+        "keywords": "frontend, all, object-fit, object, fit, CSS, adaptar, imágenes, diseño, responsive, contenido, visual, ajustar, tamaño, cover, fill, contain"
+    },
+    {
+        "title": "dominando las imagenes de fondo en css guia paso a paso",
+        "keywords": "frontend, all, css, imágenes, imagenes, imagen, background, diseño, web, estilos, background-clip, background-size, background-position, responsive, design"
+    },
+    {
+        "title": "cajas flotantes un vistazo a su versatilidad en el diseño web",
+        "keywords": "frontend, all, cajas, flotantes, float, maquetación, web, diseño, responsivo, flexible, CSS, técnicas, avanzadas, estructura, de, página"
+    },
+    {
+        "title": "tecnicas avanzadas para la creacion de diseños flexibles con flexbox",
+        "keywords": "frontend, all, flexbox, flex, contenido, flex-box, flexible, maquetación, web, diseño, responsivo, responsive, CSS, técnicas, avanzadas, flexible,  adaptable"
+    }
+];
--- js/search-lunar.js
+++ js/search-lunar.js
@@ -1,29 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const cardContainer = document.querySelector(".cards");
 
-     // Obtener el contenedor de las tarjetas
-     const cardContainer = document.querySelector(".cards");
-
-     const cards = cardContainer.querySelectorAll(".card");
- 
-     // Iterar sobre las tarjetas y asignarles el atributo data-id
-     cards.forEach((card, index) => {
-         card.setAttribute("data-id", index);
-     });
-
-    let documents = Array.from(document.querySelectorAll(".card")).map((card) => {
-        const keywords = card.getAttribute("data-keywords").split(","); // Divide las palabras clave en un array
-        const title = card.getAttribute("data-title").split(",");
-
+    let documents = entries.map((doc, index) => {
         return {
-            id: card.getAttribute("data-id"),
-            title: title,
-            keywords: keywords, // Usa el array de palabras clave
+            ...doc,
+            id: index
         };
-
     });
-
-    documents = documents.reverse();
-
 
     let idx = lunr(function () {
         this.ref("id");
@@ -35,16 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }, this);
     });
 
-
-
-
     document.querySelector('#search-form').addEventListener('submit', (event) => {
         event.preventDefault();
 
         let query = document.querySelector('#search-input').value;
 
-        let cards = document.querySelectorAll('.card');
-
+        const cards = cardContainer.querySelectorAll(".card");
         cards.forEach((card) => {
             card.style.display = 'none';
         });
@@ -53,19 +32,18 @@ document.addEventListener("DOMContentLoaded", () => {
         results.forEach((result) => {
             let card = cards[result.ref];
             card.style.display = 'flex';
-            console.log(card);
         });
+
+        document.querySelector('#search-input').value = "";
     });
 
-
-
-    let serachButtons = [
+    let searchButtons = [
         { name: "Todos", category: "all" },
         { name: "FrontEnd", category: "frontend" },
         { name: "Arduino", category: "arduino" },
     ];
 
-    serachButtons.forEach(button => {
+    searchButtons.forEach(button => {
         let btn = document.createElement("button");
         btn.classList.add("card-search");
 
@@ -78,26 +56,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function performSearch(category) {
-        let cards = document.querySelectorAll('.card');
-    
+        const cards = cardContainer.querySelectorAll(".card");
         cards.forEach(card => {
             card.style.display = "none";
         });
-    
+
         if (category === "all") {
             cards.forEach(card => {
                 card.style.display = "flex";
             });
         } else {
-            let results = idx.search("category-" + category); // Agrega el prefijo "category-" a la categoría
+            let results = idx.search(category); // Buscamos directamente por categoría
             results.forEach(result => {
                 let card = cards[result.ref];
                 card.style.display = "flex";
-                console.log(result);
             });
         }
     }
-    
-
-})
-
+});
