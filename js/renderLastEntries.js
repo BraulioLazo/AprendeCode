9bb3628ec4e1b21d4af4f5f687a141fa1d50cff4
Braulio Lazo <yobraulio23@gmail.com>
2023-09-14 12:40:29 -0400

    NUEVA ENTRADA Slider con JavaScript

--- js/renderLastEntries.js
+++ js/renderLastEntries.js
@@ -1,24 +1,24 @@
 const lastEntriesFrontend = [
     {
+        title: "Incorporando y Personalizando Videos en HTML: Una Guía Completa",
+        description: "Descubre cómo incorporar y personalizar videos en HTML eficazmente. Desde formatos hasta controles, esta guía te lleva paso a paso hacia una experiencia web óptima.",
+        imageUrl: "/frontend/incorporando-y-personalizando-videos-en-html-una-guia-completa/images/equipo-desarrolladores-aplicacion-web.webp",
+        imageAlt: "Equipo de desarrolladores trabajando en una aplicación web, con dos miembros codificando activamente mientras otros discuten temas relacionados.",
+        link: "/frontend/incorporando-y-personalizando-videos-en-html-una-guia-completa/",
+    },
+    {
+        title: "Manipulando el DOM: Creación y Adición de Elementos HTML con JavaScript",
+        description: "Descubre cómo manipular el DOM con JavaScript de manera eficiente. En esta entrada, profundizamos en la creación y adición dinámica de elementos HTML, proporcionando ejemplos claros y técnicas prácticas.",
+        imageUrl: "/frontend/manipulando-el-dom-creacion-y-adicion-de-elementos-html-con-javascript/images/desarrolladores-manipulacion-dom.webp",
+        imageAlt: "Desarrollador y desarrolladora trabajando en la manipulación del DOM utilizando JavaScript",
+        link: "/frontend/manipulando-el-dom-creacion-y-adicion-de-elementos-html-con-javascript/",
+    },
+    {
         title: "Descubre Cómo Mejorar la Comunicación Entre Funciones en JavaScript",
         description: "Aprende a mejorar la comunicación entre funciones en JavaScript para llevar tu código al siguiente nivel. Este tutorial te guiará a través de técnicas efectivas para escribir funciones más limpias y organizadas.",
         imageUrl: "/frontend/descubre-como-mejorar-la-comunicacion-entre-funciones-en-javascript/images/comunicacion-entre-funciones-javascrcript.webp",
         imageAlt: "Una pareja tumbada en cómodos sillones, la chica está escribiendo código mientras su pareja la observa; representando una buena comunicación entre funciones en JavaScript.",
         link: "/frontend/descubre-como-mejorar-la-comunicacion-entre-funciones-en-javascript/",
-    },
-    {
-        title: "Crear un Efecto Fade In Asombroso en JavaScript: Todo lo que Necesitas Saber",
-        description: "Aprende a crear un efecto Fade In asombroso usando JavaScript con nuestro tutorial detallado. Esta guía te llevará paso a paso a través del proceso para hacer que tu sitio web se destaque.",
-        imageUrl: "/frontend/crear-un-efecto-fade-in-asombroso-en-javascript/images/hombre-codificando-efecto-fade-in-javascript.webp",
-        imageAlt: "Hombre joven con audífonos sentado frente a una laptop, codificando un efecto Fade In en JavaScript.",
-        link: "/frontend/crear-un-efecto-fade-in-asombroso-en-javascript/",
-    },
-    {
-        title: "¿Cómo hacer Efecto Scroll con JavaScript?",
-        description: "Descubre cómo añadir un toque dinámico y atractivo a tu sitio web con efectos de desplazamiento (Scroll) en JavaScript. Sumérgete en este tutorial detallado que te guiará paso a paso para implementar un efecto de scroll en el menú de tu página.",
-        imageUrl: "/frontend/como-hacer-efecto-scroll-con-javascript/images/chica-en-escritorio-trabajando-en-efecto-scroll.webp",
-        imageAlt: "Chica sentada en su escritorio mirando el efecto de desplazamiento que ha implementado en su sitio web.",
-        link: "/frontend/como-hacer-efecto-scroll-con-javascript/",
     },
 ];
 
@@ -45,7 +45,7 @@ function renderLastEntries() {
     const lastEntriesFrontendContainer = document.querySelector(".latest-entries-frontend__cards-container");
     lastEntriesFrontendContainer.innerHTML = "";
 
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < lastEntriesFrontend.length; index++) {
         lastEntriesFrontendContainer.innerHTML += renderEntryContent(lastEntriesFrontend[index]);
     }
 }
