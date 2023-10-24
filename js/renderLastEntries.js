7c47b0b4aec2ec40f84715d13a015016748e0d99
Braulio Lazo <yobraulio23@gmail.com>
2023-10-24 12:05:15 -0400

    Algunas mejoras Pintas Last Entries

--- js/renderLastEntries.js
+++ js/renderLastEntries.js
@@ -22,6 +22,30 @@ const lastEntriesFrontend = [
     },
 ];
 
+const lastEntriesArduino = [
+    {
+        title: "Explorando el Corazón de Arduino: Un Recorrido por sus Componentes Esenciales",
+        description: "¡Da tus primeros pasos en el emocionante mundo de la electrónica y la programación! Explora los componentes esenciales, aprende sobre los pines digitales y analógicos, y abre un mundo lleno de posibilidades.",
+        imageUrl: "/arduino/explorando-el-corazón-de-arduino-un-recorrido-por-sus-componentes-esenciales/images/Componentes-Placa-Arduino-UNO.webp",
+        imageAlt: "Joven creando un proyecto con la placa Arduino UNO",
+        link: "/arduino/explorando-el-corazón-de-arduino-un-recorrido-por-sus-componentes-esenciales/",
+    },
+    {
+        title: "Iniciando con Arduino: Preparación del Entorno y Plataformas",
+        description: "Descubre cómo preparar tu entorno de trabajo en Arduino con esta guía introductoria. Desde plataformas virtuales hasta primeros pasos con el IDE.",
+        imageUrl: "/arduino/iniciando-con-arduino-preparacion-del-entorno-y-plataformas/images/muchacho-trabajando-robot-arduino.webp",
+        imageAlt: "Imagen de un joven trabajando en la construcción y programación de un robot utilizando Arduino.",
+        link: "/arduino/iniciando-con-arduino-preparacion-del-entorno-y-plataformas/",
+    },
+    {
+        title: "Arduino: Sumérgete en el Universo de la Creación Electrónica",
+        description: "Descubre el fascinante Universo de Arduino, una plataforma de creación electrónica que te permite explorar y crear proyectos desde tu imaginación ...",
+        imageUrl: "/arduino/arduino-sumergente-en-el-universo-de-la-creacion-electronica/images/joven-disenando-carrito-con-Arduino.webp",
+        imageAlt: "Joven diseñando un carrito en la computadora sobre el escritorio.",
+        link: "/arduino/arduino-sumergente-en-el-universo-de-la-creacion-electronica/",
+    },
+];
+
 const renderEntryContent = (cardContent) => {
     return `
     <div class="card">
@@ -41,12 +65,15 @@ const renderEntryContent = (cardContent) => {
     `;
 };
 
-function renderLastEntries() {
-    const lastEntriesFrontendContainer = document.querySelector(".latest-entries-frontend__cards-container");
-    lastEntriesFrontendContainer.innerHTML = "";
-
-    for (let index = 0; index < lastEntriesFrontend.length; index++) {
-        lastEntriesFrontendContainer.innerHTML += renderEntryContent(lastEntriesFrontend[index]);
-    }
+function renderEntries(containerSelector, entries) {
+    const container = document.querySelector(containerSelector);
+    const html = entries.map(entry => renderEntryContent(entry)).join('');
+    container.innerHTML = html;
 }
-renderLastEntries();
+
+function renderLastEntries() {
+    renderEntries(".latest-entries-frontend__cards-container", lastEntriesFrontend);
+    renderEntries(".latest-entries-arduino__cards-container", lastEntriesArduino);
+}
+
+window.addEventListener('DOMContentLoaded', renderLastEntries);
